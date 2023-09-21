391cc6cfe7dcb4af1b2cd080aaa503711d5b3b15
firmankhoiril <firmankhoiril13@gmail.com>
2023-09-21 22:58:31 +0700

    test

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import * as dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
-// import { TextLoader } from "langchain/document_loaders/fs/text";
 import { queryPineconeVectorStoreAndQueryLLM, updatedPinecone } from "./utils/pinecone.js";
 import { Pinecone } from "@pinecone-database/pinecone";
+// import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+// import { TextLoader } from "langchain/document_loaders/fs/text";
+// import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 
 dotenv.config();
 
@@ -19,14 +21,14 @@ app.get("/", async (req, res) => {
   });
 });
 
-const PORT = 5000 | "https://agile-cardigan-slug.cyclic.cloud/";
-
 app.post("/api/question", async (req, res) => {
   const { question } = req.body;
-
-  // const loader = new TextLoader("./data/multipliers as plain text.txt");
-
+  // const loader = new DirectoryLoader("./data", {
+  //   ".txt": (path) => new TextLoader(path),
+  //   ".pdf": (path) => new PDFLoader(path),
+  // });
   // const docs = await loader.load();
+
   const indexName = process.env.PINECONE_INDEX;
 
   const pinecone = new Pinecone({
